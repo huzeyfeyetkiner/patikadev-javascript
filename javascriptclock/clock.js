7cce5ea44d6221e8f3e6c668a8441075bbdb4f95
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2022-08-11 17:00:43 +0300

    clock finished

---
+++ javascriptclock/clock.js
@@ -0,0 +1,22 @@
+function showTime(){
+    const TIME = new Date();
+    let h = TIME.getHours();
+    let m = TIME.getMinutes();
+    let s = TIME.getSeconds();
+    let d = TIME.getDay();
+    let day = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+    let currentDay = day[d].toString();    
+    
+    let myClock = document.querySelector("#myClock")
+    myClock.innerHTML = h + ":" + m +":"+s +" "+currentDay;
+    setTimeout(showTime,1000);
+}
+
+
+
+let nameInfo = prompt("Adınız Nedir?:")
+let myName = document.querySelector("#myName")
+
+myName.innerHTML = nameInfo
+
+showTime();
